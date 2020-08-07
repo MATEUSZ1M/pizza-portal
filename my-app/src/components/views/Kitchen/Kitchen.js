bd2bf4e5ad8eace2adad5c868829fe1070c4b6d1
Mateusz Mikolajczyk <mikolajczykmateusz1993@gmail.com>
2020-08-07 21:46:30 +0000

    Format Waiter.js

--- my-app/src/components/views/Kitchen/Kitchen.js
+++ my-app/src/components/views/Kitchen/Kitchen.js
@@ -8,6 +8,15 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 
+const demoContent = [
+  { id: '1', order: '1', meal: 'pizza peperoni extra salami' },
+  { id: '2', order: '4', meal: 'salad' },
+  { id: '3', order: 'Online-234', meal: 'hot dog' },
+  { id: '4', order: '1', meal: 'sup' },
+  { id: '5', order: '3', meal: 'pizza salami' },
+  { id: '6', order: '4', meal: 'chesecake' },
+];
+
 const Kitchen = () => {
   return (
     <div className={styles.component}>
@@ -17,22 +26,25 @@ const Kitchen = () => {
           <TableHead>
             <TableRow>
               <TableCell>#</TableCell>
-              <TableCell>Table</TableCell>
+              <TableCell>Table/Order</TableCell>
               <TableCell>Order</TableCell>
-              <TableCell>Action</TableCell>
+              <TableCell>Status</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow component="th" scope="row">
-              <TableCell>1</TableCell>
-              <TableCell>4</TableCell>
-              <TableCell>
-                <TableRow>Pizza Peperoni</TableRow>
-              </TableCell>
-              <TableCell>
-                <Button>Done</Button>
-              </TableCell>
-            </TableRow>
+            {demoContent.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>{row.order}</TableCell>
+                <TableCell>{row.meal}</TableCell>
+                <TableCell>
+                  <Button>In preparation</Button>
+                  <Button>Done</Button>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </Paper>
